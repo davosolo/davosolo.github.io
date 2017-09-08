aed1ed5856615f465c83ba88a026b802c671a648
David Muñoz <davidmzm@gmail.com>
2017-09-08 11:26:30 -0500

    including public folder

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -8,9 +8,9 @@ exports.components = {
   "page-component---src-pages-contact-js": require("gatsby-module-loader?name=page-component---src-pages-contact-js!/Users/david/Sites/davosolo.github.io/src/pages/contact.js"),
   "page-component---src-pages-index-js": require("gatsby-module-loader?name=page-component---src-pages-index-js!/Users/david/Sites/davosolo.github.io/src/pages/index.js"),
   "page-component---src-pages-resume-js": require("gatsby-module-loader?name=page-component---src-pages-resume-js!/Users/david/Sites/davosolo.github.io/src/pages/resume.js"),
+  "page-component---src-pages-skills-js": require("gatsby-module-loader?name=page-component---src-pages-skills-js!/Users/david/Sites/davosolo.github.io/src/pages/skills.js"),
   "page-component---src-pages-examples-about-css-modules-js": require("gatsby-module-loader?name=page-component---src-pages-examples-about-css-modules-js!/Users/david/Sites/davosolo.github.io/src/pages/examples/about-css-modules.js"),
-  "page-component---src-pages-examples-counter-js": require("gatsby-module-loader?name=page-component---src-pages-examples-counter-js!/Users/david/Sites/davosolo.github.io/src/pages/examples/counter.js"),
-  "page-component---src-pages-skills-js": require("gatsby-module-loader?name=page-component---src-pages-skills-js!/Users/david/Sites/davosolo.github.io/src/pages/skills.js")
+  "page-component---src-pages-examples-counter-js": require("gatsby-module-loader?name=page-component---src-pages-examples-counter-js!/Users/david/Sites/davosolo.github.io/src/pages/examples/counter.js")
 }
 
 exports.json = {
@@ -20,10 +20,10 @@ exports.json = {
   "contact.json": require("gatsby-module-loader?name=path---contact!/Users/david/Sites/davosolo.github.io/.cache/json/contact.json"),
   "index.json": require("gatsby-module-loader?name=path---index!/Users/david/Sites/davosolo.github.io/.cache/json/index.json"),
   "resume.json": require("gatsby-module-loader?name=path---resume!/Users/david/Sites/davosolo.github.io/.cache/json/resume.json"),
+  "skills.json": require("gatsby-module-loader?name=path---skills!/Users/david/Sites/davosolo.github.io/.cache/json/skills.json"),
   "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/david/Sites/davosolo.github.io/.cache/json/404-html.json"),
   "examples-about-css-modules.json": require("gatsby-module-loader?name=path---examples-about-css-modules!/Users/david/Sites/davosolo.github.io/.cache/json/examples-about-css-modules.json"),
-  "examples-counter.json": require("gatsby-module-loader?name=path---examples-counter!/Users/david/Sites/davosolo.github.io/.cache/json/examples-counter.json"),
-  "skills.json": require("gatsby-module-loader?name=path---skills!/Users/david/Sites/davosolo.github.io/.cache/json/skills.json")
+  "examples-counter.json": require("gatsby-module-loader?name=path---examples-counter!/Users/david/Sites/davosolo.github.io/.cache/json/examples-counter.json")
 }
 
 exports.layouts = {
